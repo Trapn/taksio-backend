afe75b40ca4f10c72bdab83766748ca8787a3aaa
Steve Trap <trap.steve@hotmail.com>
2018-04-17 20:47:24 +0200

    returning array with all requests

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -11,22 +11,26 @@ class AnalyticsController extends Controller
 {
     public function responded()
     {
+        $respondedRequests = array();
         $taxiResponses = TaxiResponse::all()->where('taxi_request_id', !null);
         foreach ($taxiResponses as $taxiResponse){
-            return TaxiRequest::all()->where('id', $taxiResponse->taxi_id);
-        }      
+            $respondedRequests[] = TaxiRequest::all()->where('id', $taxiResponse->taxi_id);
+        }
+        return $respondedRequests;      
     }
 
     public function unResponded()
     {
+        $unRespondedRequests = array();
         $taxiRequests = TaxiRequest::all();
         foreach ($taxiRequests as $taxiRequest){  
             $taxiResponses = TaxiResponse::all();
             foreach ($taxiResponses as $taxiResponse){
                 if($taxiRequest->id != $taxiResponse->taxi_id){
-                    return $taxiRequest;
+                    $unRespondedRequests[] = $taxiRequest;
                 }
             }
         }
+        return $unRespondedRequests;
     }
 }
