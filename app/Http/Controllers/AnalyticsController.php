e537cfac0c328153eef67fc76aedb8af60406e75
Steve Trap <trap.steve@hotmail.com>
2018-04-17 15:38:33 +0200

    only take last 10 values

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -11,13 +11,17 @@ class AnalyticsController extends Controller
 {
     public function responded()
     {
-        $taxiResponded = TaxiResponse::where('taxi_request_id', !null);
+        $taxiResponded = TaxiResponse::where('taxi_request_id', !null)               
+        ->take(10)
+        ->get();
         return $taxiResponded;
     }
 
     public function unResponded()
     {
-        $taxiUnresponded = TaxiResponse::where('taxi_request_id', null);
+        $taxiUnresponded = TaxiResponse::where('taxi_request_id', null)
+        ->take(10)
+        ->get();
         return $taxiUnresponded;
     }
 }
