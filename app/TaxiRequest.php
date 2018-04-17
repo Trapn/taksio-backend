60e5b72eb70ee9f2e23ce136f773d9840e52d1e2
Steve Trap <trap.steve@hotmail.com>
2018-04-17 09:51:46 +0200

    changed wheel_user to wheelchair_user

--- app/TaxiRequest.php
+++ app/TaxiRequest.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaxiRequest extends Model
 {
-    protected $fillable = array('location', 'destination', 'luggage', 'wheel_user');
+    protected $fillable = array('location', 'destination', 'luggage', 'wheelchair_user');
     
 }
