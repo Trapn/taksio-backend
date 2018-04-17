f4c8ddb10b5175e5b8555deffd2b7a9897a0c85d
Steve Trap <trap.steve@hotmail.com>
2018-04-17 10:49:11 +0200

    changed the updated_at

--- app/TaxiResponse.php
+++ app/TaxiResponse.php
@@ -8,6 +8,4 @@ class TaxiResponse extends Model
 {
     protected $fillable = array('location', 'destination', 'luggage', 'wheel_user');
 
-    const CREATED_AT = 'respone_date';
-    const UPDATED_AT = 'last_update';
 }
