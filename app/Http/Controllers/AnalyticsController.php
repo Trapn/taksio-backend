c6d235344db24f2d5a5591e1ac53cb20e1fedf60
Steve Trap <trap.steve@hotmail.com>
2018-04-17 20:26:07 +0200

    split up the responded and non responded requests

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -11,17 +11,22 @@ class AnalyticsController extends Controller
 {
     public function responded()
     {
-        $taxiResponded = TaxiResponse::where('taxi_request_id', !null)               
-        ->take(10)
-        ->get();
-        return $taxiResponded;
+        $taxiResponses = TaxiResponse::all()->where('taxi_request_id', !null);
+        foreach ($taxiResponses as $taxiResponse){
+            return TaxiRequest::all()->where('id', $taxiResponse->taxi_id);
+        }      
     }
 
     public function unResponded()
     {
-        $taxiUnresponded = TaxiResponse::where('taxi_request_id', 0)
-        ->take(10)
-        ->get();
-        return $taxiUnresponded;
+        $taxiRequests = TaxiRequest::all();
+        foreach ($taxiRequests as $taxiRequest){  
+            $taxiResponses = TaxiResponse::all();
+            foreach ($taxiResponses as $taxiResponse){
+                if($taxiRequest->id != $taxiResponse->taxi_id){
+                    return $taxiRequest;
+                }
+            }
+        }
     }
 }
--- app/TaxiResponse.php
+++ app/TaxiResponse.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaxiResponse extends Model
 {
+    protected $table = 'taxi_responses';
+    
     protected $fillable = array('expected_time', 'price', 'taxi_id', 'taxi_request_id');
 
 }
