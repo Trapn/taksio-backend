c92845aadb05eaa6662d8d1416dd37b20bc90795
Steve Trap <trap.steve@hotmail.com>
2018-04-18 13:02:26 +0200

    empty safety

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -11,11 +11,13 @@ class AnalyticsController extends Controller
 {
     public function responded()
     {   
+        if(TaxiResponse::all()->where('taxi_request_id', !null) != null){
         $taxiResponses = TaxiResponse::all()->where('taxi_request_id', !null);
         foreach($taxiResponses as $taxiResponse){
             $respondedRequests[] = TaxiRequest::where('id', $taxiResponse->taxi_request_id)->get();
         }
         return $respondedRequests;
+    }
     }
 
     public function unResponded()
