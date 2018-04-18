090db3cebe45339bff1f41b70361a0c518e15f55
Steve Trap <trap.steve@hotmail.com>
2018-04-18 07:33:24 +0200

    test

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -11,14 +11,26 @@ class AnalyticsController extends Controller
 {
     public function responded()
     {   
-        $taxiResponses = TaxiResponse::where('taxi_request_id', !null)->get();
-         foreach($taxiResponses->taxi_id as $id){
-             echo $id;
-         }
+        $taxiResponses = TaxiResponse::where('taxi_request_id', !null)->first();
+        foreach($taxiResponses->taxi_id as $id){
+            echo $id;
+        }
+
     }
 
     public function unResponded()
     {
-
+        $unRespondedRequests = array();
+        $taxiRequests = TaxiRequest::all();
+        $taxiResponses = TaxiResponse::all();
+        foreach ($taxiRequests as $taxiRequest){  
+            foreach ($taxiResponses as $taxiResponse){
+                if($taxiResponse->taxi_id !== $taxiRequest->id){
+                    $unRespondedRequests[] = $taxiRequest;
+                    
+                }
+            }
+        }
+        return $unRespondedRequests;
     }
 }
